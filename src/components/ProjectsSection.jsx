aba775c31c10f5143178441d4d79597d8322dcb9
webafsanakeya <webafsakeya@gmail.com>
2025-08-28 00:20:22 +0600

    changes my project skilss

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,28 +3,28 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
-    description: "A beautiful landing page app using React and Tailwind.",
+    title: "Roommate Finder Website",
+    description: "A platform that helps individuals find compatible roommates.",
     image: "/projects/project1.png",
-    tags: ["React", "TailwindCSS", "Supabase"],
+    tags: ["React", "TailwindCSS", "Firebase", "MongoDb"],
     demoUrl: "#",
     githubUrl: "#",
   },
   {
     id: 2,
-    title: "Orbit Analytics Dashboard",
+    title: "Lost and Found Website",
     description:
-      "Interactive analytics dashboard with data visualization and filtering capabilities.",
+      "A platform designed to connect individuals who have lost personal belongings with those who may have found them.",
     image: "/projects/project2.png",
-    tags: ["TypeScript", "D3.js", "Next.js"],
+    tags: ["Full-stack applications"],
     demoUrl: "#",
     githubUrl: "#",
   },
   {
     id: 3,
-    title: "E-commerce Platform",
+    title: "Medical Camp Management System",
     description:
-      "Full-featured e-commerce platform with user authentication and payment processing.",
+      "Mern stack application with user authentication and payment processing.",
     image: "/projects/project3.png",
     tags: ["React", "Node.js", "Stripe"],
     demoUrl: "#",
