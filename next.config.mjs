258a560a76d212254ef3383565dbe2bfbe839e72
webafsanakeya <webafsakeya@gmail.com>
2025-10-05 02:57:31 +0600

    changes to next.config.mjs settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // 👈 enables static HTML export
+  images: {
+    unoptimized: true, // 👈 needed if you use <Image /> component
+  },
+};
 
 export default nextConfig;
