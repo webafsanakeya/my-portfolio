57dc54c41e606649c3f1a69b897a9adc73cd246f
Afsana Noor Keya <webafsanakeya@gmail.com>
2025-10-27 01:23:24 +0600

    Added working resume download link

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -49,16 +49,16 @@ const Header = () => {
           Contact me{" "}
           <Image src={assets.right_arrow_white} alt="" className=" w-4" />
         </motion.a>
-        <motion.a
-          initial={{ y: 30, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.6, delay: 1.2 }}
-          href="/sample-resume.pdf"
-          download
-          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white text-black"
-        >
-          My resume <Image src={assets.download_icon} alt="" className=" w-4" />
-        </motion.a>
+      <motion.a
+  initial={{ y: 30, opacity: 0 }}
+  whileInView={{ y: 0, opacity: 1 }}
+  transition={{ duration: 0.6, delay: 1.2 }}
+  href="/sample-resume.pdf"
+  download
+  className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white text-black"
+>
+  My resume <Image src={assets.download_icon} alt="" className=" w-4" />
+</motion.a>
       </div>
     </div>
   );
